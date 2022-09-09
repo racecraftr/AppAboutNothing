01300010d43c5d3579069f98cdeb570f8424ea6f
Avi Gupta <29521802+racecraftr@users.noreply.github.com>
2022-09-09 08:43:47 -0400

    initial commit.

---
+++ AppAboutNothing/Shared/AppAboutNothingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppAboutNothingApp.swift
+//  Shared
+//
+//  Created by Racecraftr on 9/9/22.
+//
+
+import SwiftUI
+
+@main
+struct AppAboutNothingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppAboutNothing/Shared/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Racecraftr on 9/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+func squareNumber(a: Int) -> Int {
+    return a * a
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
